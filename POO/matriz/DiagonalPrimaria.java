3fc17b55bf117939c82873af0ee53c1cf6b01be9
BrunoMendes/Bruno <bruninhu121nopudim@gmail.com>
2025-02-19 20:28:54 -0300

    Update DiagonalPrimaria.java

--- POO/matriz/DiagonalPrimaria.java
+++ POO/matriz/DiagonalPrimaria.java
@@ -1,25 +1,25 @@
-package matriz;
+
 public class DiagonalPrimaria {
 	public static void main(String[] args) {
-		int a[][] = new int[3][3];
-		a[0][0] = 1;
-		a[0][1] = 2;
-		a[0][2] = 3;
-		a[1][0] = 4;
-		a[1][1] = 5;
-	    a[1][2] = 6;
-	    a[2][0] = 7;
-	    a[2][1] = 8;
-	    a[2][2] = 9;
-		for(int i = 0;i<3;i++) {
-			for(int k = 0;k<3;k++) {
-				if(k == i) {
-					System.out.print(a[i][k]);
+	  int a[][] = new int[3][3];
+	  a[0][0] = 1;
+	  a[0][1] = 2;
+	  a[0][2] = 3;
+	  a[1][0] = 4;
+	  a[1][1] = 5;
+	  a[1][2] = 6;
+	  a[2][0] = 7;
+	  a[2][1] = 8;
+	  a[2][2] = 9;
+	  for(int i = 0;i<3;i++) {
+		for(int k = 0;k<3;k++) {
+		    if(k == i) {
+			System.out.print(a[i][k]);
 				}
 			}	
-				System.out.println();
+			System.out.println();
 		}
-				System.out.println("Esta é a diagonal primária da matrix");
+			System.out.println("Esta é a diagonal primária da matrix");
 	}
 }
 
